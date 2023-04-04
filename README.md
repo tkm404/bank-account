2eaccb01f2abb355b99c6447a3c0b75f826bb11a
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-04-04 14:47:18 -0700

    add Id tagging to accountHolders
    Co-Authored-By Raed alkhanbashi <raed.alkhanbashi@gmail.com>

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ Expected Output: BankAccount {userName: "Joey"}
 Test: "It should have a first deposit property, and that property should display as an integer."
 Code: let newAccount = new BankAccount("Joey", "100");
 Expected Output: BankAccount {userName: "Joey", firstDeposit: 100}
+
+//Describe: BankAccount.cashWithdraw()
+
+test: "It sould return the differnce of firstDeposit and withdraw"
+code: return this.firstDeposit -= this.withDraw
+Expected Output: he differnce of firstDeposit and withdraw
+
--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
-function BankAccount(userName, firstDeposit) {
-    this.userName = userName;
-    this.firstDeposit = parseInt(firstDeposit);
+function BankAccount() {
+    this.accountHolders = {};
+    this.currentId = 0;
 }
 
+BankAccount.prototype.addaccountId = function(accountHolder) {
+    accountHolder.id = this.assignId();
+    this.accountHolders[accountHolder.id] = accountHolder;
+};
 
-let newAccount = new BankAccount("Joey", "100");
+BankAccount.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId 
+};
+    BankAccount.prototype.cashWithdraw = function() {
+    return this.firstDeposit -= this.withDraw;
+}
 
-console.log(newAccount)
+function AccountHolder(userName, firstDeposit, withDraw) {
+    this.userName = userName;
+    this.firstDeposit = parseInt(firstDeposit);
+    this.withDraw = parseInt(withDraw);
+}
+
+let bank = new BankAccount();
+let newAccount = new AccountHolder("Joey", "100", "25");
+bank.addaccountId(newAccount);
 
